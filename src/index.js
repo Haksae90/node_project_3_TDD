6ee766c600cf8aaf73af3a9fe1bf0eeecd52482e
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-02-09 22:08:17 +0900

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 class Site {
+
     constructor() {
         this.boards = [];
     } 
 
     addBoard(board) {
-        const findBoard = this.boards.findIndex((i) => i.name === board.name);
-        if (-1 === findBoard) {
+        const findBoard = this.boards.find((i) => i.name === board.name);
+        if (findBoard) throw new Error('There is already the same board name.');
             board.addedBoard = true;
             this.boards.push(board);
-        } else {
-            throw new Error('There is already the same board name.');
-        }
     }
 
     findBoardByName(board) {
@@ -20,24 +18,20 @@ class Site {
 }
 
 class Board {
+
     constructor(board) {
-        if (board === '' || board === null) {
-            throw new Error('You must give board name');
-        }
-        this.addedBoard = false;
-        this.name = board;
-        this.article = [];
+        if (!board) throw new Error('You must give board name');
+            this.addedBoard = false;
+            this.name = board;
+            this.article = [];
     }
 
     publish(article) {
-        if (!this.addedBoard) {
-            throw new Error('Please register the board on the site');
-        } else {
+        if (!this.addedBoard) throw new Error('Please register the board on the site');
             article.id = `${this.name}-${Math.random()}`;
             article.createdDate = new Date().toISOString();
             article.comment = [];
             this.article.push(article);
-        }
     }
 
     getAllArticles() {
@@ -48,15 +42,9 @@ class Board {
 class Article {
     constructor(article) {
         const { subject, content, author } = article;
-        if (subject === '' || subject === null) {
-            throw new Error('Please write the subject');
-        }
-        if (content === '' || content === null) {
-            throw new Error('Please write the content');
-        }
-        if (author === '' || author === null) {
-            throw new Error('Please write the author');
-        }
+        if (!subject) throw new Error('Please write the subject');
+        if (!content) throw new Error('Please write the content');
+        if (!author) throw new Error('Please write the author');
         this.subject = subject;
         this.content = content;
         this.author = author;
@@ -72,16 +60,13 @@ class Article {
 }
 
 class Comment {
+
     constructor(comment) {
         const { content, author } = comment;
-        if (content === '' || content === null) {
-            throw new Error('Please write the content');
-        }
-        if (author === '' || author === null) {
-            throw new Error('Please write the author');
-        }
-        this.content = content;
-        this.author = author;
+            if (!content) throw new Error('Please write the content');
+            if (!author) throw new Error('Please write the author');
+                this.content = content;
+                this.author = author;
     }
 }
 
